e98eb876c508f895177352b08b3dee7f19ff3226
Fernando Cancino <fcancino92@gmail.com>
2022-09-23 19:05:39 -0300

    segunda entrega primer desafio

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 class Usuario {
-    constructor(nombre, apellido, libros, mascotas) {
+    constructor (nombre, apellido, libros, mascotas) {
         this.nombre = nombre
         this.apellido = apellido
         this.libros = libros
         this.mascotas = mascotas
     }
+    
     getFullName()
-    addMascota(mascotas) {
-        const newMascota =
-        this.mascotas.push
-    }
+
+   addMascotas (mascotas) {
+        const newMascotas = { mascotas: mascotas}
+        this.mascotas.push (newMascotas)
+   }
+
     countMascotas(mascotas) {
         mascotas.length
     }
+
     addBook(nombre, autor) {
         const newBook = {nombre: nombre, autor: autor}
         this.libros.push (newBook)
     }
+
     getBookNames(libros) {
         this.libros.map (libros)
     }
@@ -31,5 +36,8 @@ const usuario1 = new Usuario(
     }],
     ["gallina", "gallo"])
 
-usuario1.addBook ("La rueda del tiempo", "Roberet Jordan")
+usuario1.addBook ("La rueda del tiempo", "Robert Jordan")
 usuario1.addMascota ("chancho")
+usuario1.countMascotas ()
+usuario1.getBookNames ()
+usuario1.getFullName ()
