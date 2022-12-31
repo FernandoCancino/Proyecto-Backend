8832cb2aaf141bbd8bd40873b790b8e6788a8892
Fernando Cancino <fcancino92@gmail.com>
2022-12-31 07:12:58 -0300

    entrega 15 parte 1

--- server.js
+++ server.js
@@ -1,110 +1,91 @@
-const express = require('express');
-const options = require("./config/dbConfig");
-const {productsRouter, products} = require('./routes/products');
-const handlebars = require('express-handlebars');
-const {Server} = require("socket.io");
-const {normalize, schema} = require("normalizr");
+import express from "express";
+import session from "express-session";
+import MongoStore from "connect-mongo";
+import cookieParser from "cookie-parser";
+import handlebars from "express-handlebars";
+import {Server} from "socket.io";
+import path from "path";
+import {fileURLToPath} from 'url';
+import parseArgs from "minimist";
+import cluster from "cluster";
+import os from "os";
 
-const Contenedor = require("./managers/contenedorProductos");
-const ContenedorChat = require('./managers/contenedorChat');
-const ContenedorSql = require("./managers/contenedorSql");
+import { dbOptions } from "./config/dbConfig.js";
+import { productRouter } from "./routes/api/products.js";
+import { clientRouter } from "./routes/web/clientRoutes.js";
+import { authRouter } from "./routes/web/authRoutes.js";
+import { productsSocket } from "./routes/ws/products.js";
+import { chatSocket } from "./routes/ws/chat.js";
 
-//service
-// const productosApi = new Contenedor("productos.txt");
-const productosApi = new ContenedorSql(options.mariaDB, "products");
-const chatApi = new ContenedorChat("chat.txt");
-// const chatApi = new ContenedorSql(options.sqliteDB,"chat");
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-//server
+//captura argumentos
+const options = {alias:{m:"mode", p:"port"}, default:{mode:"FORK", port:8080}};
+const objArguments = parseArgs(process.argv.slice(2), options);
+const MODO = objArguments.mode;
+const PORT = objArguments.port;
+// console.log("modo",MODO, "port", PORT);
+
+
+//Express server config
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
-app.use(express.static(__dirname+'/public'))
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(__dirname+"/public"));
 
-//configuracion template engine handlebars
-app.engine('handlebars', handlebars.engine());
+//Configuracion template engine handlebars
+app.engine(".hbs",handlebars.engine({extname: '.hbs'}));
 app.set('views', __dirname+'/views');
-app.set('view engine', 'handlebars');
+app.set("view engine", ".hbs");
 
-//normalizacion
-//creamos los esquemas.
-//esquema del author
-const authorSchema = new schema.Entity("authors",{}, {idAttribute:"email"});
+//Configuración de la sesión
+app.use(cookieParser());
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: dbOptions.mongoDBAtlas.mongoUrl
+    }),
+    secret:"ClaveSecreta",
+    resave: false,
+    saveUninitialized: false,
+    cookie:{
+        maxAge: 60000 //10min
+    }
+}));
 
-//esquema mensaje
-const messageSchema = new schema.Entity("messages", {author: authorSchema});
-
-//creamos nuevo objeto para normalizar la informacion
-// {
-//     id:"chatHistory",
-//     messages:[
-//         {},{},{}
-//     ]
-// }
-//esquema global para el nuevo objeto
-const chatSchema = new schema.Entity("chat", {
-    messages:[messageSchema]
-}, {idAttribute:"id"});
-
-//aplicar la normalizacion
-//crear una funcion que la podemos llamar para normalizar la data
-const normalizarData = (data)=>{
-    const normalizeData = normalize({id:"chatHistory", messages:data}, chatSchema);
-    return normalizeData;
-};
-
-const normalizarMensajes = async()=>{
-    const results = await chatApi.getAll();
-    const messagesNormalized = normalizarData(results);
-    // console.log(JSON.stringify(messagesNormalized, null,"\t"));
-    return messagesNormalized;
-}
-
-// routes
+// Server routes
+// Api routes
+app.use('/api/productos',productRouter);
 //view routes
-app.get('/', async(req,res)=>{
-    res.render('home')
-})
-
-app.get('/productos',async(req,res)=>{
-    res.render('products',{products: await productosApi.getAll()})
-})
-
-//api routes
-app.use('/api/products',productsRouter)
+app.use(clientRouter);
+app.use(authRouter);
 
 
-//express server
-const server = app.listen(8080,()=>{
-    console.log('listening on port 8080')
-})
+//Express server
+if(MODO === "CLUSTER" && cluster.isPrimary) {
+    console.log("modo cluster")
+    const numCPUS = os.cpus().length; //numero de nucleos del procesador
+    for(let i=0; i<numCPUS;i++){
+        cluster.fork(); //creamos los subprocesos
+    }
 
-
-
-//websocket server
-const io = new Server(server);
-
-//configuracion websocket
-io.on("connection",async(socket)=>{
-    //PRODUCTOS
-    //envio de los productos al socket que se conecta.
-    io.sockets.emit("products", await productosApi.getAll())
-
-    //recibimos el producto nuevo del cliente y lo guardamos con filesystem
-    socket.on("newProduct",async(data)=>{
-        await productosApi.save(data);
-        //despues de guardar un nuevo producto, enviamos el listado de productos actualizado a todos los sockets conectados
-        io.sockets.emit("products", await productosApi.getAll())
-    })
-
-    //CHAT
-    //Envio de todos los mensajes al socket que se conecta.
-    io.sockets.emit("messages", await normalizarMensajes());
-
-    //recibimos el mensaje del usuario y lo guardamos en el archivo chat.txt
-    socket.on("newMessage", async(newMsg)=>{
-        console.log(newMsg);
-        await chatApi.save(newMsg);
-        io.sockets.emit("messages", await normalizarMensajes());
+    cluster.on("exit",(worker)=>{
+        console.log(`El subproceso ${worker.process.pid} falló`);
+        cluster.fork();
     });
-})
+
+} else{
+    const server = app.listen(PORT,()=>{
+        console.log(`listening on port ${PORT} on process ${process.pid}`);
+    });
+
+    //Websocket server
+    const io = new Server(server);
+    //configuracion websocket
+    io.on("connection",async(socket)=>{
+        // console.log('Nuevo cliente conectado!');
+        //PRODUCTOS
+        productsSocket(socket, io.sockets);
+        //CHAT
+        chatSocket(socket, io.sockets);
+    });
+}
