d21c23cf6d6f04f4be9c71b16085e4073ac47cd8
Fernando Cancino <fcancino92@gmail.com>
2023-02-18 10:40:22 -0300

    entrega 20

---
+++ src/services/user.service.js
@@ -0,0 +1,19 @@
+import {
+    getApiDao
+} from "../model/index.js";
+import {
+    options
+} from "../config/config.js";
+
+const {
+    UserDaoContainer,
+    ProductDaoContainer
+} = await getApiDao(options.server.dbType);
+
+export const getUsers = async () => {
+    return await UserDaoContainer.getAll();
+};
+
+export const saveUser = async (body) => {
+    return await UserDaoContainer.save(body);
+};
